44bbe9af39c4da832d4174c540b237d4e029a22c
HannaVelychko <anna.ua19@gmail.com>
2022-09-07 21:26:22 +0200

    Changed city

--- src/Weather.js
+++ src/Weather.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 export default function Weather() {
   function handleResponse(response) {
-    alert(`The weather in New York ${response.data.main.temp}°C`);
+    alert(`The weather in Wroclaw is ${response.data.main.temp}°C`);
   }
   let apiKey = "094780c710fa4efd669f0df8c3991927";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Wroclawk&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
 
-  return <h2>Hello from Weather</h2>;
+  return <h2>Hello from Weather-react</h2>;
 }
